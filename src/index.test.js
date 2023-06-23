2dd94383a31dba330a8fae056983cb1c9a7eca5b
Kaushik Dhameliya <dhameliyakaushik13@gmail.com>
2023-06-23 18:36:01 +0530

    build(unit-testing): replace mocha with vitest
    As vitest is a new and fastest unit testing library, replaced mocha integration with vitest
    
    BREAKING CHANGE: codecov could be not working as expected with nyc

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,25 @@
-import {expect} from 'chai'
-import names from '.'
+import { it, expect, describe } from "vitest";
+import names from ".";
 
-describe('names', () => {
-  it('should have a list of all available names', () => {
-    expect(names.all).to.satisfy(isArrayOfStrings)
-  })
+describe("all function should be available", () => {
+  it("should have a list of all available names", () => {
+    expect(isArrayOfStrings(names.all)).toBeTruthy();
+    expect(isArrayOfStrings(names.all)).toMatchSnapshot()
+  });
 
-  it('should allow me to get a random name from the list', () => {
-    expect(names.random()).to.satisfy(isIncludedIn(names.all))
-  })
-})
+  it("should allow me to get a random name from the list", () => {
+    expect(isIncludedIn(names.random)).toBeTruthy();
+  });
+
+  it("should allow me to get a random name from the list", () => {
+    expect(isIncludedIn(names.all)("John")).toBeFalsy();
+  });
+});
 
 function isArrayOfStrings(array) {
-  return array.every(i => typeof i === 'string')
+  return array.every((i) => typeof i === "string");
 }
 
 function isIncludedIn(array) {
-  return item => array.includes(item)
+  return (item) => array.includes(item);
 }
